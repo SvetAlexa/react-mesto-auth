00346a63922b001fb79951a51c76d11c3c68903a
SvetAlexa <dazzle@bk.ru>
2023-08-20 20:38:18 +0300

    исправить функционал меню бургера

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,21 +14,14 @@ export default function Header({ email, onSignOut }) {
         setIsBurgerMenuOpen(false);
     }
 
+    function handleSignOut() {
+        onSignOut();
+        setIsBurgerMenuOpen(false);
+    }
+
     return (
-        <header className={`header page__container-header ${isBurgerMenuOpen ? 'header_type_burger' : ''}`}>
-            <div className={`nav ${isBurgerMenuOpen ? 'nav_is-opened' : 'nav_is_closed'}`}>
-                <p className='nav__user'>{email}</p>
-                <button className='nav__button-logout' onClick={onSignOut}>Выйти</button>
-            </div>
+        <header className={` ${isBurgerMenuOpen ? 'header page__container-header header_type_burger' : 'header page__container-header'}`}>
             <img className="header__logo" src={Logo} alt="логотип Место Россия" />
-            {isBurgerMenuOpen ? <button type="button" className="menu-burger__close-button" onClick={handleEscButtonClick}></button>
-                :
-                <div className='menu-burger' onClick={handleBurgerMenuClick}>
-                    <span className='menu-burger__line'></span>
-                    <span className='menu-burger__line'></span>
-                    <span className='menu-burger__line'></span>
-                </div>
-            }
             <Routes>
                 <Route path='/sign-up' element={
                     <Link to="/sign-in" className="header__nav">Войти</Link>}
@@ -37,10 +30,24 @@ export default function Header({ email, onSignOut }) {
                     <Link to="/sign-up" className="header__nav">Регистрация</Link>}
                 />
                 <Route path='/' element={
-                    <div className='nav nav_is_hidden'>
-                        <p className='nav__user'>{email}</p>
-                        <button className='nav__button-logout' onClick={onSignOut}>Выйти</button>
-                    </div>
+                    <>
+                        <div className='nav nav_is_hidden'>
+                            <p className='nav__user'>{email}</p>
+                            <button className='nav__button-logout' onClick={handleSignOut}>Выйти</button>
+                        </div>
+                        <div className={`nav ${isBurgerMenuOpen ? 'nav_is-opened' : 'nav_is_closed'}`}>
+                            <p className='nav__user'>{email}</p>
+                            <button className='nav__button-logout' onClick={handleSignOut}>Выйти</button>
+                        </div>
+                        {isBurgerMenuOpen ? <button type="button" className="menu-burger__close-button" onClick={handleEscButtonClick}></button>
+                            :
+                            <div className='menu-burger' onClick={handleBurgerMenuClick}>
+                                <span className='menu-burger__line'></span>
+                                <span className='menu-burger__line'></span>
+                                <span className='menu-burger__line'></span>
+                            </div>
+                        }
+                    </>
                 }
                 />
             </Routes>
